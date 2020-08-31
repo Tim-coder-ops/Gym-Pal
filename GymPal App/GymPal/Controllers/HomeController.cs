8e276fd4af519a79a1b63f145f4bc931701a6e04
Tim-coder-ops <timweigel63@gmail.com>
2020-08-30 18:25:15 -0700

    Added new Gym Pal App data due to GITHUB errors

---
+++ GymPal App/GymPal/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GymPal.Models;
+
+namespace GymPal.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("home")]
+        public IActionResult Home()
+        {
+            return View("Index");
+        }
+
+
+         [HttpGet("login")]
+         public IActionResult login()
+         {
+             return View("login");
+         }
+
+         [HttpGet("register")]
+         public IActionResult register()
+         {
+             return View("register");
+         }
+
+         [HttpGet("userLogin")]
+         public IActionResult userLogin()
+         {
+             return View("UserHome");
+         }
+
+         [HttpGet("logWorkout")]
+         public IActionResult LogWorkout()
+         {
+             return View("LogWorkout");
+         }
+
+         [HttpGet("aboutUs")]
+         public IActionResult AboutUs()
+         {
+             return View("AboutUs");
+         }
+
+
+
+        
+    }
+}
