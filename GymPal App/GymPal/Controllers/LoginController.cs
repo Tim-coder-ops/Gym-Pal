bdb7a4d750b26b7a9ff09061a2c33128c4204688
Tim-coder-ops <timweigel63@gmail.com>
2020-08-30 18:17:53 -0700

    Readded GymPal App Due to GITHUB Errors

---
+++ GymPal App/GymPal/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GymPal.Models;
+
+namespace GymPal.Controllers
+{
+     public class LoginController : Controller
+     {
+         public IActionResult Login()
+         {
+             return View();
+         }
+
+        //  [HttpGet("newregister")]
+        //   public IActionResult register()
+        //   {
+        //       return View("register");
+        //   }
+     }
+}
