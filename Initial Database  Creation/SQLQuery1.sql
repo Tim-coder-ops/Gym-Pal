ad1bb4a279ba21fcef6b4f4b025ecc990afa78a5
Tim-coder-ops <timweigel63@gmail.com>
2020-09-19 22:20:26 -0700

    Updated Initial Seed

--- Initial Database  Creation/SQLQuery1.sql
+++ Initial Database  Creation/SQLQuery1.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS [Excercises]
-CREATE TABLE [Excercises] (
+DROP TABLE IF EXISTS [Exercises]
+CREATE TABLE [Exercises] (
   [ID] INT PRIMARY KEY IDENTITY,
   [BodyPart]  VARCHAR(20),
   [Excercise] VARCHAR(20),
@@ -8,7 +8,7 @@ CREATE TABLE [Excercises] (
 
 DROP TABLE IF EXISTS [Workout]
 CREATE TABLE [Workout] (
-  ID INT IDENTITY(1,1) PRIMARY KEY,
+  ID INT IDENTITY PRIMARY KEY,
   [Workout Number] INT,
   [UserID] INT FOREIGN KEY REFERENCES Users(ID),
   [Date] Date
@@ -16,7 +16,7 @@ CREATE TABLE [Workout] (
 
 DROP TABLE IF EXISTS [Sets]
 CREATE TABLE [Sets] (
-  ID INT IDENTITY(1,1) PRIMARY KEY,
+  ID INT IDENTITY PRIMARY KEY,
   [ExerciseID] INT FOREIGN KEY REFERENCES Exercises(ID),
   [NumberOfReps] INT,
   [Weight] INT,
@@ -40,45 +40,31 @@ CREATE TABLE [Users] (
   [Password] NVARCHAR(50)
 );
 
-INSERT INTO [Excercises] (ExerciseID, BodyPart, Excercise, TypeOfWorkout)
-VALUES (1, 'Chest', 'Bench Press', 'strength'),
-(2, 'Back', 'Lat Pulldowns',  'strength'),
-(3, 'legs', 'Squats', 'strength')
+--Seeding Data
 
-INSERT INTO [Workout History] ([Workout Number], ExerciseID, SetID, UserID,  NumberOfReps, Weight, Date)
-VALUES (1, 1, 1, 1, 12, 185, '2020-07-20'),
-(1, 1, 2, 1, 10, 205, '2020-07-20'),
-(1, 1, 3, 1, 10, 205, '2020-07-20'),
-(1, 1, 4, 1, 7, 225, '2020-07-20'),
-(1, 2, 1, 1, 12, 180, '2020-07-20'),
-(1, 2, 2, 1, 10, 200, '2020-07-20'),
-(1, 2, 3, 1, 10, 200, '2020-07-20'),
-(1, 2, 4, 1, 6, 220, '2020-07-20')
+INSERT INTO [Exercises] ( BodyPart, Excercise, TypeOfWorkout)
+VALUES ( 'Chest', 'Bench Press', 'strength'),
+( 'Back', 'Lat Pulldowns',  'strength'),
+( 'legs', 'Squats', 'strength')
 
-INSERT INTO [Progress] (UserID, Date, Weight) 
+INSERT INTO [Workout] ([Workout Number], UserID, Date)
+VALUES (1,1, '2020-07-20')
+
+INSERT INTO Sets (ExerciseID, NumberOfReps, Weight, [Workout ID])
+VALUES(1, 12, 205, 1),
+(1, 15, 205, 1),
+(1, 10, 225, 1),
+(1, 8, 245, 1)
+
+INSERT INTO [Progress] (UserID, Date, BodyWeight) 
 VALUES (1, '2020-07-20', 215)
 
-INSERT INTO [Users] (UserID, [FirstName], [LastName])
-VALUES (1, 'Tim', 'Weigel'),
-(2, 'John', 'Smith')
+INSERT INTO [Users] ( [FirstName], [LastName], Email, Password)
+VALUES ( 'Tim', 'Weigel', 'example@SomeThing.com', 'Password')
 
-SELECT FirstName + ' ' + LastName AS Name,
-	BodyPart, Excercise,
-	[Workout Number], 
-	SetID,
-	Weight,
-	NumberOfReps, 
-	Date
-FROM Excercises E
-JOIN [Workout History] WH
-ON E.ExerciseID = WH.ExerciseID
-JOIN Users U
-ON U.UserID = WH.UserID
-
-SELECT FirstName, 
-	LastName, 
-	Date, 
-	Weight
-FROM Users U
-JOIN Progress P
-ON U.UserID = P.UserID
+--Ensuring Tables are populated
+SELECT * FROM Users
+SELECT * FROM Progress
+SELECT * FROM Sets
+SELECT * FROM Workout
+SELECT * FROM Exercises
