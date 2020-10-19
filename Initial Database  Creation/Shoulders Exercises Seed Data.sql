4a957245a59ab990ca50c26c9d43ccc15cd0a9b9
Tim-coder-ops <timweigel63@gmail.com>
2020-10-18 18:54:38 -0700

    Added Shoulder Exercises Seed Data

---
+++ Initial Database  Creation/Shoulders Exercises Seed Data.sql
@@ -0,0 +1,32 @@
+
+INSERT INTO Exercises Values
+('Shoulders', 'Arnold Press (Dumbbell)', 'Strength'),
+('Shoulders', 'Face Pull (Cable)', 'Strength'),
+('Shoulders', 'Front Raise (Band)', 'Strength'),
+('Shoulders', 'Front Raise (Barbell)', 'Strength'),
+('Shoulders', 'Front Raise (Cable)', 'Strength'),
+('Shoulders', 'Front Raise (Dumbbell)', 'Strength'),
+('Shoulders', 'Front Raise (Plate)', 'Strength'),
+('Shoulders', 'Lateral Raise (Band)', 'Strength'),
+('Shoulders', 'Lateral Raise (Cable)', 'Strength'),
+('Shoulders', 'Lateral Raise (Dumbbell)', 'Strength'),
+('Shoulders', 'Lateral Raise (Machine)', 'Strength'),
+('Shoulders', 'Overhead Press (Barbell)', 'Strength'),
+('Shoulders', 'Overhead Press (Cable)', 'Strength'),
+('Shoulders', 'Overhead Press (Dumbbell)', 'Strength'),
+('Shoulders', 'Overhead Press (Smith Machine)', 'Strength'),
+('Shoulders', 'Push Press', 'Strength'),
+('Shoulders', 'Reverse Fly (Cable)', 'Strength'),
+('Shoulders', 'Reverse Fly (Dumbbell)', 'Strength'),
+('Shoulders', 'Reverse Fly (Machine)', 'Strength'),
+('Shoulders', 'Seated Overhead Press (Barbell)', 'Strength'),
+('Shoulders', 'Seated Overhead Press (Dumbbell)', 'Strength'),
+('Shoulders', 'Shoulder Press (Machine)', 'Strength'),
+('Shoulders', 'Shoulder Press (Plate Loaded)', 'Strength'),
+('Shoulders', 'Shrug (Barbell)', 'Strength'),
+('Shoulders', 'Shrug (Dumbbell)', 'Strength'),
+('Shoulders', 'Upright Row (Dumbbell)', 'Strength')
+
+
+
+
